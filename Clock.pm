40a191eae93a3a8095f02a7684fefff6be1115fc
H.Merijn Brand <merijn@nb09.(none)>
2008-01-02 15:11:13 +0100

    Upped copyright to 2008

--- Clock.pm
+++ Clock.pm
@@ -674,7 +674,7 @@ Thanks to all who have given me feedback.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 1999-2007 H.Merijn Brand
+Copyright (C) 1999-2008 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
