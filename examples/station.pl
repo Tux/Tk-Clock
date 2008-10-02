2a2b244158efa82ec0fedf95f247084b29c967bb
H.Merijn Brand <merijn@nb09.procura.nl>
2008-10-02 16:07:11 +0200

    Station clock is now resizeable and the ticks don't touch the edges

--- examples/station.pl
+++ examples/station.pl
@@ -3,18 +3,40 @@
 use Tk;
 use Tk::Clock;
 
+my @bw = qw( Black White );
+
+use Getopt::Long qw(:config bundling nopermute);
+GetOptions (
+    "r|rev|wb|white-on-black" => sub { @bw = reverse @bw },
+    ) or die "usage: station.pl [--white-on-black]\n";
+
 my $m = MainWindow->new;
-my $c = $m->Clock (-background => "White")->pack (-expand => 1, -fill => "both");
+
+$m->configure (
+    -foreground	=> $bw[0],
+    -background	=> $bw[1],
+    );
+
+my $c = $m->Clock (
+    -background	=> $bw[1],
+    -relief	=> "flat",
+  )->pack (
+    -expand	=> 1,
+    -fill	=> "both",
+    -padx	=> "10",
+    -pady	=> "10",
+    );
 $c->config (
     useDigital	=> 0,
     useAnalog	=> 1,
     secsColor	=> "Red",
-    handColor	=> "Black",
-    tickColor	=> "Black",
+    handColor	=> $bw[0],
+    tickColor	=> $bw[0],
     tickFreq	=> 1,
     tickDiff	=> 1,
     handCenter	=> 1,
     anaScale	=> 500,
     );
+$c->config (anaScale => 0);
 
 MainLoop;
