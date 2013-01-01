a4141f580e15ef3b1a8ac55e2856c393b53b8e65
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2013-01-01 22:24:22 +0100

    © 2013

--- Clock.pm
+++ Clock.pm
@@ -1022,7 +1022,7 @@ Thanks to all who have given me feedback and weird ideas.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 1999-2012 H.Merijn Brand
+Copyright (C) 1999-2013 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
