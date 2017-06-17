388736bc3f35fd95e1df4308db5ee6480da42264
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-06-17 09:22:44 +0200

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -58,14 +58,14 @@ requires:
     POSIX:               0
 recommends:     
     Tk:                  804.033
-    Encode:              2.88
+    Encode:              2.90
 configure_requires:
     ExtUtils::MakeMaker: 0
 test_requires:
-    Test::More:          0.88
+    Test::More:          0.90
     Test::NoWarnings:    0
 test_recommends:
-    Test::More:          1.302075
+    Test::More:          1.302085
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/Tk-Clock
