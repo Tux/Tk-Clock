44180683808ed893d7b1250e88f91cf50c24ec67
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2020-04-09 10:26:55 +0200

    It's 2020

--- Clock.pm
+++ Clock.pm
@@ -1166,7 +1166,7 @@ Thanks to all who have given me feedback and weird ideas.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 1999-2019 H.Merijn Brand
+Copyright (C) 1999-2020 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
